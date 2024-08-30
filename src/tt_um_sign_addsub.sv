de7b3ec66771fb97a940d5592d3e1bbf9b701a51
chiranjitpatel <chiranjitpatel08@gmail.com>
2024-08-30 23:57:07 +0530

    updated SV file
    Co-Authored-By: Vivek Adi <vivek.adishesha@gmail.com>

--- src/tt_um_sign_addsub.sv
+++ src/tt_um_sign_addsub.sv
@@ -16,21 +16,21 @@ module tt_um_sign_addsub (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
+
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   signed_addsub_4 A1 (	.a(ui_in[3:0]),
 						.b(ui_in[7:4]), 
 						.res_signed(uo_out[3:0]),
 						.sign(uio_in[1:0]));
 
+   // All output pins must be assigned. If not used, assign to 0.
+   assign uo_out[7:4] = 0;
+   assign uio_out = 0;
 
-
-  assign uio_out[7:2] = 0;
-  assign uo_out[7:4] = 0;
+   // List all unused inputs to prevent warnings
+  // assign uio_in[7:2] = 0;
   assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in[7:2], 1'b0};
 
 endmodule
 
